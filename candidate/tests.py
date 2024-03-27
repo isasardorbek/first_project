b9b97f458b26dc4a838e2e364430f147404c4433
Sardorbek Isakulov <sardorbekisakulov720@gmail.com>
2024-03-27 21:26:24 +0500

    with test

--- candidate/tests.py
+++ candidate/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+
 
 # Create your tests here.
+@override_settings(LOGIN_URL="/login/")
+class LoginTestCase(TestCase):
+    def test_login(self):
+        response = self.client.get("/candidate/1/", follow=True)
+        self.assertRedirects(response, "/login/?next=/candidate/1")
